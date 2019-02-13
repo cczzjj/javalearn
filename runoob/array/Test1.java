71f0e9d9233c8062b54e346bcece392cd02a2787
cczzjj <1026067356@qq.com>
2019-02-13 22:43:29 +0800

    array

---
+++ runoob/array/Test1.java
@@ -0,0 +1,21 @@
+package runoob.array;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Test1 {
+    public static void main(String[] args) {
+        String s[][] = new String[2][];
+        s[0] = new String[2];
+        s[1] = new String[3];
+        s[0][0] = new String("Good");
+        s[0][1] = new String("Luck");
+        s[1][0] = new String("to");
+        s[1][1] = new String("you");
+        s[1][2] = new String("!");
+
+        for (String[] e: s) {
+            System.out.println(Arrays.binarySearch(e, "to"));
+        }
+    }
+}
---
+++ runoob/array/TestArray.java
@@ -0,0 +1,60 @@
+package runoob.array;
+
+public class TestArray {
+    public static void main(String[] args) {
+        // 数组大小
+        int size = 10;
+        // 定义数组
+        double[] myList = new double[size];
+        myList[0] = 5.6;
+        myList[1] = 4.5;
+        myList[2] = 3.3;
+        myList[3] = 13.2;
+        myList[4] = 4.0;
+        myList[5] = 34.33;
+        myList[6] = 34.0;
+        myList[7] = 45.45;
+        myList[8] = 99.993;
+        myList[9] = 11123;
+        // 计算所有元素的总和
+        double total = 0;
+        for (int i = 0; i < size; i++) {
+            total += myList[i];
+        }
+        System.out.println("总和为： " + total);
+
+        double[] myList2 = {1.9, 2.9, 3.4, 3.5};
+
+        // 打印所有数组元素
+        for (int i = 0; i < myList2.length; i++) {
+            System.out.println(myList2[i] + " ");
+        }
+        // 计算所有元素的总和
+        double total2 = 0;
+        for (int i = 0; i < myList2.length; i++) {
+            total2 += myList2[i];
+        }
+        System.out.println("Total is " + total2);
+        // 查找最大元素
+        double max = myList2[0];
+        for (int i = 1; i < myList2.length; i++) {
+            if (myList2[i] > max) max = myList2[i];
+        }
+        System.out.println("Max is " + max);
+
+        double[] myList3 = {1.9, 2.9, 3.4, 3.5};
+
+        // 打印所有数组元素
+        for (double element: myList3) {
+            System.out.println(element);
+        }
+
+        printArray(new int[]{3, 1, 2, 6, 4, 2});
+    }
+
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+}
