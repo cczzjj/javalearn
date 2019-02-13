6952953cb98f70bd54311bbd97b4b51d43a1bd16
cczzjj <1026067356@qq.com>
2019-02-13 19:20:54 +0800

    modifier_types

---
+++ runoob/modifier_types/SuperClass.java
@@ -0,0 +1,12 @@
+package runoob.modifier_types;
+
+public abstract class SuperClass{
+    abstract void m(); // 抽象方法
+}
+
+class SubClass extends SuperClass{
+    // 实现抽象方法
+    void m(){
+
+    }
+}
