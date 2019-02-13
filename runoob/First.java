8c8c59efff9491fbc03875393662f622ee6e6df1
cczzjj <1026067356@qq.com>
2019-02-13 14:35:17 +0800

    FreshJuiceTest

--- runoob/First.java
+++ runoob/First.java
@@ -1,6 +1,4 @@
-package runoob;
-
-public class first {
+public class First {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
---
+++ runoob/FreshJuiceTest.java
@@ -0,0 +1,14 @@
+package runoob;
+
+class FreshJuice {
+    enum FreshJuiceSize {SMALL, MEDIUM, LARGE}
+
+    FreshJuiceSize size;
+}
+
+public class FreshJuiceTest {
+    public static void main(String[] args) {
+        FreshJuice juice = new FreshJuice();
+        juice.size = FreshJuice.FreshJuiceSize.MEDIUM;
+    }
+}
