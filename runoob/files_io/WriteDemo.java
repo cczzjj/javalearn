772211f5bfbd19e65be2367160287eb1f9482ef5
cczzjj <1026067356@qq.com>
2019-02-14 11:59:14 +0800

    files io

---
+++ runoob/files_io/WriteDemo.java
@@ -0,0 +1,10 @@
+package runoob.files_io;
+
+public class WriteDemo {
+    public static void main(String args[]) {
+        int b;
+        b = 'A';
+        System.out.write(b);
+        System.out.write('\n');
+    }
+}
